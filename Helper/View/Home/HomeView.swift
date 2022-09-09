cd19622b5b0dd5e33528f5f55e386711c5b52d8c
Macostik <yura-granchenko@yandex.ru>
2022-09-09 20:15:46 +0300

    feat(tabBar): animation for tab bar

--- Helper/View/Home/HomeView.swift
+++ Helper/View/Home/HomeView.swift
@@ -7,69 +7,39 @@
 
 import SwiftUI
 
-struct TabItem: Identifiable, Hashable {
-    let id = UUID().uuidString
-    let tag: String
-    let image: String
-}
-
 struct HomeView: View {
     @State var selectedItem = "house"
-    @Namespace private var animation
-    let tabs = [
-        TabItem(tag: "house",
-                image: "house"),
-        TabItem(tag: "search",
-                image: "magnifyingglass"),
-        TabItem(tag: "person",
-                image: "person")
-    ]
     var body: some View {
         VStack {
             TabView(selection: $selectedItem, content: {
                 ZStack {
-                    Color.mainBGColor
+                    Color.mainBGColor.ignoresSafeArea()
                     Text("Home")
                         .foregroundColor(.white)
                 }
                 .tag("house")
                 ZStack {
-                    Color.mainBGColor
+                    Color.mainBGColor.ignoresSafeArea()
                     Text("Search")
                         .foregroundColor(.white)
                 }
-                .tag("search")
+                .tag("magnifyingglass")
                 ZStack {
-                    Color.mainBGColor
+                    Color.mainBGColor.ignoresSafeArea()
                     Text("Profile")
                         .foregroundColor(.white)
                 }
                 .tag("person")
             })
             .tabViewStyle(.automatic)
-            .ignoresSafeArea()
-            HStack {
-                ForEach(tabs, id: \.self) { tab in
-                    Button {
-                        withAnimation {
-                            selectedItem = tab.tag
-                        }
-                    } label: {
-                        VStack(spacing: 5) {
-                            Image(systemName: tab.image)
-                                .font(Font.poppins(.bold, size: 18))
-                                .foregroundColor(Color.mainColor)
-                            Circle()
-                                .frame(width: 10, height: 10)
-                                .matchedGeometryEffect(id: "tab", in: animation)
-                                .foregroundColor(selectedItem == tab.tag ? Color.mainColor : Color.clear)
-                        }
-                    }
-                }
-                .frame(maxWidth: .infinity)
-            }
-            .padding()
-            .background(RoundedRectangle(cornerRadius: 12).foregroundColor(Color.white.opacity(0.1)))
+            TabBarView(selectedTab: $selectedItem)
+            .background(
+                Color.white.opacity(0.1)
+            )
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.mainColor, lineWidth: 1)
+            )
             .padding()
         }
         .padding(.bottom)
@@ -84,3 +54,65 @@ struct HomeView_Previews: PreviewProvider {
     }
 }
 
+struct TabBarView: View {
+    @Binding var selectedTab: String
+    @Namespace private var animation
+    var body: some View {
+        HStack {
+            TabBarItem(animation: animation,
+                    image: "house",
+                    selectedTab: $selectedTab)
+            TabBarItem(animation: animation,
+                    image: "magnifyingglass",
+                    selectedTab: $selectedTab)
+            TabBarItem(animation: animation,
+                    image: "person",
+                    selectedTab: $selectedTab)
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+    }
+}
+
+struct TabBarItem: View {
+    var animation: Namespace.ID
+    var image: String
+    @Binding var selectedTab: String
+    var body: some View {
+        ZStack {
+            Button {
+                withAnimation {
+                    selectedTab = image
+                }
+            } label: {
+                VStack {
+                    Image(systemName: image)
+                        .font(Font.poppins(.bold, size: 18))
+                        .foregroundColor(Color.mainColor)
+                }
+            }
+            .frame(maxWidth: .infinity)
+            if selectedTab == image {
+                Triangle()
+                    .fill(Color.mainColor)
+                    .frame(width: 10, height: 5)
+                    .matchedGeometryEffect(id: "Tab", in: animation)
+                    .foregroundColor(selectedTab == image ? Color.mainColor : Color.clear)
+                    .offset(y: 25)
+            }
+        }
+    }
+}
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
